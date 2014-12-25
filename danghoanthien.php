f568b5c151740fa50c0d7a04c33efbf91a4f61c6
dinhquybk <nguyendinhquy94@gmail.com>
2014-12-25 13:17:58 +0700

    Create danghoanthien.php

---
+++ danghoanthien.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link rel="stylesheet" style="text/css" href ="css/cc.css" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>dang hoan thien</title>
+</head>
+<body>
+<h2>Đang trong quá trình hoàn thiện. Mong bạn thông cảm.</h2>
+</br> 
+<div class="chuahoanthien" ><a title="trang chủ" href="cc.php">Quay lại trang chủ</a>
+</div>
+</body>
+</html>
